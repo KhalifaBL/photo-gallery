298217b3ab0c82bd4e01494e76eab973602cb57d
Khalifa Ben Lazrek <khalifalazreg@gmail.com>
2020-02-10 09:41:37 +0100

    Updates

---
+++ precache-manifest.dbf375abb8106ee0f24cad2f520daf4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11cbe42e8dfc0a1fb98664ebfd6efad0",
+    "url": "/photo-gallery/index.html"
+  },
+  {
+    "revision": "e6baae916aa74aecbd76",
+    "url": "/photo-gallery/static/css/main.41368f35.chunk.css"
+  },
+  {
+    "revision": "48bf471692a44bb69b09",
+    "url": "/photo-gallery/static/js/2.32519995.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/photo-gallery/static/js/2.32519995.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6baae916aa74aecbd76",
+    "url": "/photo-gallery/static/js/main.bf2ffce8.chunk.js"
+  },
+  {
+    "revision": "059e4171d1f0e62d8dc1",
+    "url": "/photo-gallery/static/js/runtime-main.48bc4bfa.js"
+  }
+]);
